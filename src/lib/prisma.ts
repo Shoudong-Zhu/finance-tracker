d1c6077a9e3ebf2c7b06c5c94924efb0d7f5cd35
Shoudong Zhu <93493532+Shoudong-Zhu@users.noreply.github.com>
2025-04-30 00:29:33 -0400

    update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,14 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+};
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    // Optional: Log queries in development
-    // log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['query', 'error', 'warn'],
+});
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
